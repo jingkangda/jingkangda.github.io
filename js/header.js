e27ca198bf18b3c47f5e8456ae48bfccc0b2a184
jingkangda <yzy131719@163.com>
2022-11-14 20:43:48 +0800

    1

---
+++ js/header.js
@@ -0,0 +1,1 @@
+var Header = '<div class="index_head"><a href="../index.html" class="web-font">首页</a><a href="../html/match.html" class="web-font">比赛</a><a href="../html/equip.html" class="web-font">装备</a>\n    <a href="../html/news.html" class="web-font">新闻</a>'
